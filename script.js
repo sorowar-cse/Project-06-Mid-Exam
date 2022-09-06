5f720af0aeffcd732065d8961c9765951ef8fc18
Sorowar Mahabub <sorowarmahabub.bsc.cse.iiuc@gmail.com>
2022-09-06 21:39:24 +0600

    Fully Updated

--- script.js
+++ script.js
@@ -2,29 +2,36 @@ const searchProducts = () => {
     fetch('https://fakestoreapi.com/products')
         .then(res => res.json())
         .then(data => showDetails(data))
-
 }
 
 
 const showDetails = (products) => {
     const details = document.getElementById('display-card');
-    products.forEach(element => {
+    // console.log(products)
+
+    for (let index = 0; index < 9; index++) {
+        const element = products[index];
         console.log(element)
+
+        // products.forEach(element => {
+        //     console.log(element)
+
+        // return ratings 
         const ratingStar = ratings(element.rating.rate);
 
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
 
-         <div class="card ms-4 border-0 shadow  h-100 product">
-           <div class="p-5">
-           <img src="${element.image}" class="card-img-top" alt="..."  height=150 >
+         <div class="card ms-2 border-0 shadow  h-100 product">
+           <div class="p-1">
+           <img src="${element.image}" class="card-img-top rounded-circle" alt="..."  height=150 >
            </div>
            <div class="card-body">
              <h5 class="card-title text-center">${element.title}</h5>
              <p class="card-text">
              </p>
-             <h5 class="card-text text-center fw-bold">$<span class="sp fw-bold">${element.price}</span></h5>
+             <h3 class="card-text text-center fw-bold">$<span class="sp fw-bold">${element.price}</span></h3>
              <h5 class="card-text text-center"><span class="sp fw-bold">${ratingStar}</span></h5>
            </div>
            <div class="footer mx-auto">
@@ -37,7 +44,7 @@ const showDetails = (products) => {
         details.appendChild(div);
 
 
-    });
+    };
 
 
 
@@ -55,10 +62,18 @@ const addToCard = (id, price, newPrice) => {
 const total = () => {
     const price = parseFloat(document.getElementById('price').innerText);
     const deliver = parseFloat(document.getElementById('delivery-charge').innerText);
-    const total = price + deliver;
-    document.getElementById('total').innerText = total
-
+    const shipping = parseFloat(document.getElementById('shipping-charge').innerText);
+    const total = price + deliver + shipping;
+    document.getElementById('total').innerText = total;
+    tax(total);
 }
+
+const tax = (total) => {
+    const taxadd= (total * 0.15);
+    document.getElementById('tax').innerText = taxadd;
+    document.getElementById('intotal').innerText = taxadd + total;
+}
+
 
 const updatePrice = (price) => {
     const oldPrice = document.getElementById('price').innerText;
@@ -66,6 +81,7 @@ const updatePrice = (price) => {
     const newPrice = price + oldPriceFloat;
     document.getElementById('price').innerText = newPrice.toFixed(2);
     DeliveryCharge(newPrice);
+    ShippingCharge(newPrice);
 }
 
 const DeliveryCharge = (newPrice) => {
@@ -73,50 +89,54 @@ const DeliveryCharge = (newPrice) => {
     if (newPrice <= 500) {
         return document.getElementById('delivery-charge').innerText = 0;
     }
-    if (newPrice > 500 && newPrice < 800) {
-        document.getElementById('delivery-charge').innerText = 50;
-    }
-    else if (newPrice >= 800) {
+    if (newPrice > 500 && newPrice <= 800) {
         document.getElementById('delivery-charge').innerText = 100;
     }
+    else if (newPrice > 800 && newPrice <= 1000) {
+        document.getElementById('delivery-charge').innerText = 150;
+    }
+    else if (newPrice >= 1000) {
+        document.getElementById('delivery-charge').innerText = 200;
+    }
+}
 
-
+const ShippingCharge = (newPrice) => {
+    let ShippingCharge;
+    if (newPrice <= 500) {
+        return document.getElementById('shipping-charge').innerText = 0;
+    }
+    if (newPrice > 500 && newPrice <= 800) {
+        document.getElementById('shipping-charge').innerText = 100;
+    }
+    else if (newPrice > 800 && newPrice <= 1000) {
+        document.getElementById('shipping-charge').innerText = 150;
+    }
+    else if (newPrice >= 1000) {
+        document.getElementById('shipping-charge').innerText = 200;
+    }
 }
 
 const ratings = (rate) => {
     if (rate >= 4) {
-        return star = ` <h3><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> ${rate}</h3>`
+        return star = ` <h3><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> <br> ${rate}</h3>`
     }
     else if (rate >= 3 && rate < 4) {
-        return star = ` <h3><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> ${rate}</h3>`
+        return star = ` <h3><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> <br> ${rate}</h3>`
     }
     else if (rate >= 2 && rate < 3) {
-        return star = ` <h3><i class="fas fa-star"></i><i class="fas fa-star"></i> ${rate}</h3>`
+        return star = ` <h3><i class="fas fa-star"></i><i class="fas fa-star"></i> <br> ${rate}</h3>`
     }
     else {
-        return star = ` <h3><i class="fas fa-star"></i> ${rate}</h3>`
+        return star = ` <h3><i class="fas fa-star"></i> <br> ${rate}</h3>`
     }
 
 }
 
 
-const orderProducts = () => {
-    const details = document.getElementById('details');
-    details.textContent = '';
-    const totalPrice = document.getElementById('total').innerText;
-    // if(totalPrice>=500){
-    //   return document.getElementById('tax').innerText=100;
-    // }
-    // else if(totalPrice<500){
-    //   return document.getElementById('tax').innerText=200;
-    // }
-    const div = document.createElement('div');
-    div.classList.add('shopping')
-    div.innerHTML = `<h4>Your total Shopping : $${totalPrice}</h4>
-   <p>Thank You!<br><span style="color:chocolate"><span>Sorowar's Shoping Zone</span></p>
-   `
 
-    details.appendChild(div)
+const orderProducts = () => {
+    const final= document.getElementById('intotal').innerText;
+    alert("Dear Sir," + "\n" + "You have to pay: " + final + "\n" + "Thank You!" + "\n" + "Regrads-" + "\n" + "Sorowar's Shopping Zone" );
 }
 
 
